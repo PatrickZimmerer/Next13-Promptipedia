6762d0eebc482fb381ef13a997304a1b678c5f6d
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-06-20 21:21:10 +0200

    added view to show Prompts in PromptCardList

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,7 +1,53 @@
-import React from 'react';
+'use client';
+import { useState, useEffect } from 'react';
+
+import PromptCard from './PromptCard';
+
+const PromptCardList = ({ data, handleTagClick }) => {
+	console.log(data);
+	return (
+		<div className="mt-16 prompt_layout">
+			{data.map((post) => (
+				<PromptCard key={post._id} post={post} handleTagClick={handleTagClick} />
+			))}
+		</div>
+	);
+};
 
 function Feed() {
-	return <div>Feed</div>;
+	const [searchText, setSearchText] = useState('');
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		const fetchPosts = async () => {
+			const response = await fetch('/api/prompt');
+			const data = await response.json();
+			setPosts(data);
+			console.log(posts);
+		};
+
+		fetchPosts();
+	}, []);
+
+	function handleSearchChange(e) {
+		console.log(e);
+	}
+	return (
+		<section className="feed">
+			<form className="relative w-full justify-center items-center">
+				<input
+					className="search_input peer"
+					type="text"
+					placeholder="Search for a tag or a username"
+					value={searchText}
+					onChange={handleSearchChange}
+					required
+				/>
+			</form>
+
+			<PromptCardList data={posts} handleTagClick={() => {}} />
+		</section>
+	);
 }
 
 export default Feed;
