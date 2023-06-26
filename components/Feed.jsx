452f380abf5a925b5dc1923363c15d953facae38
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-06-26 19:17:38 +0200

    fixed cahcing error in feed

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -19,7 +19,9 @@ function Feed() {
 
 	useEffect(() => {
 		const fetchPosts = async () => {
-			const response = await fetch('/api/prompt');
+			const response = await fetch('/api/prompt', {
+				next: { revalidate: 10, cache: 'no-store' },
+			});
 			const data = await response.json();
 			setPosts(data);
 		};
