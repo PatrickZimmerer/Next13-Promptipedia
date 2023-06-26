d9c0c456c43c4ec81042c60ba0f552c5aeb4966a
Patrick Zimmerer <p.zimmerer@east-young.de>
2023-06-26 19:17:16 +0200

    added editPromptPage

---
+++ app/update-prompt/page.jsx
@@ -0,0 +1,62 @@
+'use client';
+import { useState, useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import Form from '@components/Form';
+
+function EditPrompt() {
+	const router = useRouter();
+	const searchParams = useSearchParams();
+	const promptId = searchParams.get('id'); // get prompt id from router url
+
+	const [post, setPost] = useState({
+		prompt: '',
+		tag: '',
+	});
+	const [submitting, setSubmitting] = useState(false);
+
+	useEffect(() => {
+		const getPromptDetails = async () => {
+			const response = await fetch(`/api/prompt/${promptId}`);
+			const data = await response.json();
+			setPost({
+				prompt: data.prompt,
+				tag: data.tag,
+			});
+		};
+		if (promptId) getPromptDetails();
+	}, [promptId]);
+
+	async function updatePrompt(e) {
+		e.preventDefault();
+		setSubmitting(true);
+		if (!promptId) return alert('PromptId not found');
+		try {
+			const response = await fetch(`/api/prompt/${promptId}`, {
+				method: 'PATCH',
+				body: JSON.stringify({
+					prompt: post.prompt,
+					tag: post.tag,
+				}),
+			});
+			if (response.ok) {
+				router.push('/');
+			}
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setSubmitting(false);
+		}
+	}
+
+	return (
+		<Form
+			type="Edit"
+			post={post}
+			setPost={setPost}
+			submitting={submitting}
+			handleSubmit={updatePrompt}
+		></Form>
+	);
+}
+
+export default EditPrompt;
